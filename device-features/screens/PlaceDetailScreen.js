ba88a6f71ab1e655a81eac23f75e252392d3eab0
Sławomir <ruudn7@wp.pl>
2020-04-26 20:23:59 +0200

    initail app commit and first screen files

---
+++ device-features/screens/PlaceDetailScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const PlaceDetailScreen = props => {
+
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default PlaceDetailScreen;
