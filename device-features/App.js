3eb43df17aca6a4d47c7954a1b162955aa97fca4
Sławomir <ruudn7@wp.pl>
2020-04-28 19:34:51 +0200

    add db SQLite to project

--- device-features/App.js
+++ device-features/App.js
@@ -6,6 +6,16 @@ import ReduxThunk from 'redux-thunk';
 import PlacesNavigator from './navigation/PlacesNavigation';
 import placesReducer from './store/places-reducer';
 
+import { init } from './helpers/db'
+
+init()
+  .then(() => {
+    console.log('db init')
+  }).catch(err => {
+    console.log('db init fail')
+    console.log(err)
+  });
+
 const rootReducer = combineReducers({
   places: placesReducer
 })
--- device-features/store/places-actions.js
+++ device-features/store/places-actions.js
@@ -1,11 +1,32 @@
+import * as FileSystem from 'expo-file-system';
+
 export const ADD_PLACE = 'ADD_PLACE';
 
 export const addPlace = (title, image) => {
-    return {
-        type: ADD_PLACE,
-        placeData: {
-            title: title,
-            image: image
+    return async dispatch => {
+        const fileName = image.split('/').pop();
+        const newPatch = FileSystem.documentDirectory + fileName;
+
+        try {
+            dispatch({
+                type: ADD_PLACE,
+                placeData: {
+                    title: title,
+                    image: image
+                }
+            })
+        } catch (err) {
+            console.log(err);
+            throw err;
         }
+
+        dispatch({
+            type: ADD_PLACE,
+            placeData: {
+                title: title,
+                image: newPatch
+            }
+        })
     }
+
 }
